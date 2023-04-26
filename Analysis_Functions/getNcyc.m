3d2ee28b949d63ff4711cc691ee392029611b3aa
Jeremy Johnston <jjohns@jeremys-mbp-3.aw4.unh.edu>
2023-04-26 14:17:29 -0400

    adds and updates to analysis functions

--- Analysis_Functions/getNcyc.m
+++ Analysis_Functions/getNcyc.m
@@ -1,7 +1,8 @@
-function SCD = getSCD(path,savepath,date_range,valid_snow,valid_land,min_count,calc_SP)
-%getSCD Returns the snow cover duration (SCD, defined by input variable 
-% resolution) considering an input date range. Requires an input folder of 
-% gridded snow cover data. Currently, only works for Tiff/GeoTiff files.
+function Ncyc = getNcyc(path,savepath,date_range,valid_snow,valid_land,min_count)
+%getNcyc Returns the number of snow covered (on) to non-snow covered (off) 
+% cycles considering an input date range (see 'date_range'). Requires an 
+% input folder of gridded snow cover data. Currently, works for 
+% Tiff/GeoTiff files.
 %
 % INPUTS:
 % path - path to folder containing binary snow cover files
@@ -13,22 +14,18 @@ function SCD = getSCD(path,savepath,date_range,valid_snow,valid_land,min_count,c
 % valid_land - specify the variable in the .tif file representing bare land
 %   pixels, all others will be considered as no data (default == 0)
 % min_count - set the minimum number of files (as a proportion) in a given 
-%   timeseries to still return outputs. Also must include a value indicative
-%   of a full data period (Default = [0.95 365])
-% calc_SP - setting to determine if to also compute the snow persistence
-%   (SP), which is a unitless proportion of snow cover during the input
-%   period. Default is 1 (save SP), set to 0 to only store SCD
+%   timeseries to still return outputs. Also must include a value 
+%   indicative of a full data period (default = [0.95 365 (days)])
 %
 % OUTPUTS:
-% SCD - for 2D spatial inputs, will return .tif files with snow cover
-%   duration counts, saved to the specified 'savepath.' Will also store the
-%   most recently calculated SCD as a variable. Note: If calc_SP is set to 
-%   1, will also save the SP to the 'savepath.' -1 is assigned as the no 
-%   data values, while SP is rescaled from 0 - 1 to 0 - 1000
+% Ncyc - for 2D spatial inputs, will return .tif files with snow cover
+%   cycle counts, saved to the specified 'savepath.' Will also store the
+%   most recently calculated Ncyc as a variable. 255 is assigned as the no 
+%   data value
 %
 %
-% Note: the larger the dataset, the slower processing (e.g., takes ~30 
-% minutes to process a single year at ~1km)
+% Note: the larger the dataset, the slower processing (e.g., takes ~15 
+% minutes to process a single snow year at ~1km)
 
 
 %error handling
@@ -39,21 +36,15 @@ elseif nargin == 2
     valid_snow = 1;
     valid_land = 0;
     min_count = [0.95 365];
-    calc_SP = 1;
 elseif nargin == 3
     valid_snow = 1;
     valid_land = 0;
     min_count = [0.95 365];
-    calc_SP = 1;
 elseif nargin == 4
     valid_land = 0;
     min_count = [0.95 365];
-    calc_SP = 1;
 elseif nargin == 5
     min_count = [0.95 365];
-    calc_SP = 1;
-elseif nargin == 6
-    calc_SP = 1;
 end
 
 %extract timestamps from each file (assumes consistent naming structure)
@@ -64,6 +55,10 @@ for i = 1:length(dt)
     f = split(file,'_');
     dt(i) = datetime(str2double(f{4}),str2double(f{5}),str2double(f{6}));
 end
+
+%ensure files are sorted in chronological order
+[dt,I] = sort(dt);
+files = files(I);
 
 %check for the date_range input type and build index specifying file ranges
 %to count snow cover durations
@@ -103,10 +98,6 @@ if isa(date_range,"double") && date_range == 1 %use the hemisphere specific snow
                 var = file_idx(ii);
                 [D,R] = readgeoraster([path file_ii]);
 
-                %identify locations of snow and land
-                snow_idx = int16(D == valid_snow);
-                land_idx = int16(D == valid_land);
-
                 %initialize counting matrices, and increment counts
                 if ii == 1
 
@@ -120,61 +111,47 @@ if isa(date_range,"double") && date_range == 1 %use the hemisphere specific snow
                     
                     %stores total number of snow classifications by
                     %hemisphere
-                    SCD = zeros([size(D,1) size(D,2)],'int16');
+                    Ncyc = zeros([size(D,1) size(D,2)],'uint8');
 
-                    %stores total number of valid land or snow classifications
-                    VALID = zeros([size(D,1) size(D,2)],'int16');
+                    %create mask identifying locations of no data pixels
+                    snow_idx = (D == valid_snow);
+                    land_idx = (D == valid_land);
+                    mask = ~(snow_idx | land_idx);
 
-                    %check file_idx to determine which values to increment
-                    if var == 1 % == 1, only increment NH counts
-
-                        %set all values below equator to 0
-                        snow_idx(SH_rows(1):SH_rows(2),:) = 0;
-                        land_idx(SH_rows(1):SH_rows(2),:) = 0;
-                        SCD = SCD + snow_idx;
-                        VALID = VALID + snow_idx + land_idx;
-                                             
-                    elseif var == 2 % == 2, only increment SH counts
-
-                        %set all values above equator to 0
-                        snow_idx(NH_rows(1):NH_rows(2),:) = 0;
-                        land_idx(NH_rows(1):NH_rows(2),:) = 0;
-                        SCD = SCD + snow_idx;
-                        VALID = VALID + snow_idx + land_idx;
-                        
-                    elseif var == 3 % == 3, increment both
-
-                        %increment all locations
-                        SCD = SCD + snow_idx;
-                        VALID = VALID + snow_idx + land_idx;
-                        
-                    end
+                    %identify locations with snow cover in the first
+                    %timestep
+                    SCA_prev = snow_idx;
               
                 %increment counts
                 else
 
+                    %checks locations that previously had snow and currently do not
+                    snow_on_off = (SCA_prev) & (D == valid_land);
+
+                    %reset previous to current time step and continue
+                    SCA_prev = (D == valid_snow);
+
                     %check file_idx to determine which values to increment
                     if var == 1 % == 1, only increment NH counts
 
                         %set all values below equator to 0
-                        snow_idx(SH_rows(1):SH_rows(2),:) = 0;
-                        land_idx(SH_rows(1):SH_rows(2),:) = 0;
-                        SCD = SCD + snow_idx;
-                        VALID = VALID + snow_idx + land_idx;
-                                             
+                        snow_on_off(SH_rows(1):SH_rows(2),:) = 0;
+
+                        %keep running sum of cycles, only increment NH
+                        Ncyc = Ncyc + uint8(snow_on_off);
+                                               
                     elseif var == 2 % == 2, only increment SH counts
 
                         %set all values above equator to 0
-                        snow_idx(NH_rows(1):NH_rows(2),:) = 0;
-                        land_idx(NH_rows(1):NH_rows(2),:) = 0;
-                        SCD = SCD + snow_idx;
-                        VALID = VALID + snow_idx + land_idx;
+                        snow_on_off(NH_rows(1):NH_rows(2),:) = 0;
+
+                        %keep running sum of cycles, only increment SH
+                        Ncyc = Ncyc + uint8(snow_on_off);
                         
                     elseif var == 3 % == 3, increment both
 
                         %increment all locations
-                        SCD = SCD + snow_idx;
-                        VALID = VALID + snow_idx + land_idx;
+                        Ncyc = Ncyc + uint8(snow_on_off);
                         
                     end
                        
@@ -191,19 +168,9 @@ if isa(date_range,"double") && date_range == 1 %use the hemisphere specific snow
 
         %save outputs
         f = split(file_ii,'_');
-        fname = ['SCD_' f{1} '_' f{2} '_SY' num2str(year_list(i)) '-' num2str(year_list(i + 1)) '_' f{end}];
-        SCD(VALID == 0) = -1;
-        geotiffwrite([savepath fname],SCD,R);
-            
-        %compute SP and save, if user selected
-        if calc_SP == 1
-            SP = single(SCD)./single(VALID);
-            %rescale 0 (0%) - 1000 (100%)
-            SP = int16(SP .* 1000);
-            SP(VALID == 0) = -1;
-            fname = ['SP_' f{1} '_' f{2} '_SY' num2str(year_list(i)) '-' num2str(year_list(i + 1)) '_' f{end}];
-            geotiffwrite([savepath fname],SP,R);
-        end
+        fname = ['Ncyc_' f{1} '_' f{2} '_SY' num2str(year_list(i)) '-' num2str(year_list(i + 1)) '_' f{end}];
+        Ncyc(mask) = 255;
+        geotiffwrite([savepath fname],Ncyc,R);
 
     end    
 
@@ -232,28 +199,32 @@ elseif isa(date_range,"double") && date_range == 2 %use the calendar year
                 file_ii = files_i(ii).name; disp(file_ii);
                 [D,R] = readgeoraster([path file_ii]);
 
-                %identify locations of snow and land
-                snow_idx = int16(D == valid_snow);
-                land_idx = int16(D == valid_land);
-
                 %initialize counting matrices, and increment counts
                 if ii == 1
 
-                    %stores total number of snow classifications
-                    SCD = zeros([size(D,1) size(D,2)],'int16');
+                    %stores total number of snow classifications by
+                    %hemisphere
+                    Ncyc = zeros([size(D,1) size(D,2)],'uint8');
 
-                    %stores total number of valid land or snow classifications
-                    VALID = zeros([size(D,1) size(D,2)],'int16');
+                    %create mask identifying locations of no data pixels
+                    snow_idx = (D == valid_snow);
+                    land_idx = (D == valid_land);
+                    mask = ~(snow_idx | land_idx);
 
-                    %increment all locations
-                    SCD = SCD + snow_idx;
-                    VALID = VALID + snow_idx + land_idx;
+                    %identify locations with snow cover in the first
+                    %timestep
+                    SCA_prev = snow_idx;
 
                 else
 
-                    %increment all locations
-                    SCD = SCD + snow_idx;
-                    VALID = VALID + snow_idx + land_idx;
+                    %checks locations that previously had snow and currently do not
+                    snow_on_off = (SCA_prev) & (D == valid_land);
+
+                    %reset previous to current time step and continue
+                    SCA_prev = (D == valid_snow);
+
+                    %keep running sum of cycles, only increment NH
+                    Ncyc = Ncyc + uint8(snow_on_off);
 
                 end
 
@@ -267,19 +238,9 @@ elseif isa(date_range,"double") && date_range == 2 %use the calendar year
 
         %save outputs
         f = split(file_ii,'_');
-        fname = ['SCD_' f{1} '_' f{2} '_' num2str(year_list(i)) '_' f{end}];
-        SCD(VALID == 0) = -1;
-        geotiffwrite([savepath fname],SCD,R);
-            
-        %compute SP and save, if user selected
-        if calc_SP == 1
-            SP = single(SCD)./single(VALID);
-            %rescale 0 (0%) - 1000 (100%)
-            SP = int16(SP .* 1000);
-            SP(VALID == 0) = -1;
-            fname = ['SP_' f{1} '_' f{2} '_' num2str(year_list(i)) '_' f{end}];
-            geotiffwrite([savepath fname],SP,R);
-        end
+        fname = ['Ncyc_' f{1} '_' f{2} '_' num2str(year_list(i)) '_' f{end}];
+        Ncyc(mask) = 255;
+        geotiffwrite([savepath fname],Ncyc,R);
 
     end
     
@@ -301,28 +262,32 @@ elseif isa(date_range,'datetime') %uses the specified date range
             file_i = files_i(i).name; disp(file_i);
             [D,R] = readgeoraster([path file_i]);
 
-            %identify locations of snow and land
-            snow_idx = int16(D == valid_snow);
-            land_idx = int16(D == valid_land);
-
             %initialize counting matrices, and increment counts
             if i == 1
 
-                %stores total number of snow classifications
-                SCD = zeros([size(D,1) size(D,2)],'int16');
+                %stores total number of snow classifications by
+                %hemisphere
+                Ncyc = zeros([size(D,1) size(D,2)],'uint8');
 
-                %stores total number of valid land or snow classifications
-                VALID = zeros([size(D,1) size(D,2)],'int16');
+                %create mask identifying locations of no data pixels
+                snow_idx = (D == valid_snow);
+                land_idx = (D == valid_land);
+                mask = ~(snow_idx | land_idx);
 
-                %increment all locations
-                SCD = SCD + snow_idx;
-                VALID = VALID + snow_idx + land_idx;
+                %identify locations with snow cover in the first
+                %timestep
+                SCA_prev = snow_idx;
 
             else
 
-                %increment all locations
-                SCD = SCD + snow_idx;
-                VALID = VALID + snow_idx + land_idx;
+                %checks locations that previously had snow and currently do not
+                snow_on_off = (SCA_prev) & (D == valid_land);
+
+                %reset previous to current time step and continue
+                SCA_prev = (D == valid_snow);
+
+                %keep running sum of cycles, only increment NH
+                Ncyc = Ncyc + uint8(snow_on_off);
 
             end
 
@@ -338,19 +303,9 @@ elseif isa(date_range,'datetime') %uses the specified date range
     d1 = date_range(1); d1.Format = 'uuuu-MM-dd';
     d2 = date_range(2); d2.Format = 'uuuu-MM-dd';
     f = split(file_i,'_');
-    fname = ['SCD_' f{1} '_' f{2} '_' char(d1) '_' char(d2) '_' f{end}];
-    SCD(VALID == 0) = -1;
-    geotiffwrite([savepath fname],SCD,R);
-
-    %compute SP and save, if user selected
-    if calc_SP == 1
-        SP = single(SCD)./single(VALID);
-        %rescale 0 (0%) - 1000 (100%)
-        SP = int16(SP .* 1000);
-        SP(VALID == 0) = -1;
-        fname = ['SP_' f{1} '_' f{2} '_' char(d1) '_' char(d2) '_' f{end}];
-        geotiffwrite([savepath fname],SP,R);
-    end
+    fname = ['Ncyc_' f{1} '_' f{2} '_' char(d1) '_' char(d2) '_' f{end}];
+    Ncyc(mask) = 255;
+    geotiffwrite([savepath fname],Ncyc,R);
 
 %catch errors
 else
